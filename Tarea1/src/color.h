d678f5eb11d46bc305901630b99d6f8bb26b1dfb
Alan Vladimir Alonso Sanchez <alan_vladimir@ciencias.unam.mx>
2026-09-10 18:59:33 -0600

    Me falto la clase de color jaja

--- Tarea1/src/color.h
+++ Tarea1/src/color.h
@@ -4,6 +4,7 @@
 #include "interval.h"
 #include "header.h"
 #include "vec3.h"
+#include <cmath>
 
 
 using color = vec3;
@@ -18,7 +19,10 @@ using color = vec3;
  */
 inline double linear_to_gamma(double linear_component)
 {
-    //TODO
+    if (linear_component>0) {
+        return std::sqrt(linear_component);
+    }
+    return 0;
 }
 
 void write_color(std::ostream& out, const color& pixel)
